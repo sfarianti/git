5dff07274f7f3e9cf2ecffa2bad0edf5da5fad8d
Arya.PK <aryakusuma0703@gmail.com>
2024-10-02 14:32:04 +0700

    fix number

--- resources/views/auth/admin/management_system/assign_event_index.blade.php
+++ resources/views/auth/admin/management_system/assign_event_index.blade.php
@@ -209,6 +209,7 @@
                     "title": "No",
                     "render": function (data, type, row, meta) {
                     return meta.row + meta.settings._iDisplayStart + 1;
+                    // stest
                 }
 
                 },
