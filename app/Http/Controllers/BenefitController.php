89cad95d84390e146dda653a752853c41a4a4a4f
raflisetiawan <dev.raflisetiawan@gmail.com>
2024-10-16 11:22:22 +0700

    Fix: Memperbaiki authorisasi dari fitur benefit di halaman paper / makalah inovasi

--- app/Http/Controllers/BenefitController.php
+++ app/Http/Controllers/BenefitController.php
@@ -20,31 +20,34 @@ use Illuminate\Support\Facades\Mail;
 class BenefitController extends Controller
 {
     //
-    public function createBenefitAdmin(){
+    public function createBenefitAdmin()
+    {
         // dd(Auth::user());
         $data = Paper::join('teams', 'papers.team_id', '=', 'teams.id')
-        ->select(
+            ->select(
                 'papers.id as paper_id',
                 'teams.id as team_id',
                 'team_name',
                 'company_code',
-                'innovation_title')
-        ->get();
+                'innovation_title'
+            )
+            ->get();
         return view('auth.admin.benefit.index', ['data' => $data]);
     }
-    public function createBenefitUser($id){
+    public function createBenefitUser($id)
+    {
 
         $row = Paper::join('teams', 'papers.team_id', '=', 'teams.id')
-        ->select(
-            'papers.id as paper_id',
-            'teams.id as team_id',
-            'teams.company_code as company_code',
-            'financial',
-            'file_review',
-            'potential_benefit',
-            'non_financial',
-            'papers.status_rollback',
-            DB::raw('
+            ->select(
+                'papers.id as paper_id',
+                'teams.id as team_id',
+                'teams.company_code as company_code',
+                'financial',
+                'file_review',
+                'potential_benefit',
+                'non_financial',
+                'papers.status_rollback',
+                DB::raw('
                 CASE
                     WHEN papers.status = \'not finish\' THEN \'nf\'
                     WHEN papers.status = \'not accepted\' THEN \'na\'
@@ -54,33 +57,53 @@ class BenefitController extends Controller
                     WHEN papers.status = \'rejected by innovation admin\' THEN \'rbia\'
                     ELSE papers.status
                 END as status')
-        )
-        ->where('papers.id', $id)->first();
+            )
+            ->where('papers.id', $id)->first();
 
-        $file_content = null;
-        if ($row->file_review) {
-            $file_content = Storage::get('public/' . $row->file_review);
-        }
-        $benefit_custom = CustomBenefitFinancial::where('company_code', $row->company_code)
-        ->get()->keyBy('id')->toArray();
+            $is_owner = PvtMember::where('employee_id', auth()->user()->employee_id)
+            ->where('team_id', $row->team_id) // Cek apakah user adalah bagian dari tim yang terkait dengan paper
+            ->where('status', ['leader', 'member']) // Atau status lain yang menunjukkan pemilik benefit
+            ->exists(); // Gunakan exists() untuk cek keberadaan pemilik benefit
+
+
+            $file_content = null;
+            if ($row->file_review) {
+                // Perbaiki path ke file sesuai dengan direktori yang benar
+                $file_path = $row->file_review;
+
+                // Cek apakah file benar-benar ada sebelum mencoba mengambilnya
+                if (Storage::disk('public')->exists($file_path)) {
+                    // Ambil konten file
+                    $file_content = Storage::disk('public')->get($file_path);
+                }
+            }
+
+            $benefit_custom = CustomBenefitFinancial::where('company_code', $row->company_code)
+            ->get()->keyBy('id')->toArray();
         // $benefit_custom = $benefit_custom_query;
 
-        foreach($benefit_custom as $bencus){
+        foreach ($benefit_custom as $bencus) {
             $pvt = PvtCustomBenefit::where('custom_benefit_financial_id', $bencus['id'])
                 ->where('paper_id', $id)
                 ->first();
-            if($pvt){
+            if ($pvt) {
                 $benefit_custom[$bencus['id']]['value'] = $pvt->getValueFormattedAttribute();
-            }else{
+            } else {
                 $benefit_custom[$bencus['id']]['value'] = null;
             }
         }
-
-        // dd($row);
-        return view('auth.user.benefit.index', compact('row', 'benefit_custom' ,'file_content'));
+        $gm = PvtMember::where('team_id', $row->team_id)->where('status', 'gm')->first();
+        $gmName = null;
+        if($gm !== null){
+            $gmName = User::where('employee_id', $gm->employee_id)->select('name')->first();
+        }else{
+            $gmName = null;
+        }
+        return view('auth.user.benefit.index', compact('row', 'benefit_custom', 'file_content', 'is_owner', 'gmName'));
     }
 
-    public function storeBenefitUser(Request $request, $id){
+    public function storeBenefitUser(Request $request, $id)
+    {
         //dd($request->all());
 
         $record = Paper::with('team')->findOrFail($id);
@@ -94,9 +117,9 @@ class BenefitController extends Controller
         $potensi_replikasi = $request->input('potensi_replikasi');
         $non_financial = $request->non_financial;
 
-        if($request->file('file_review')){
+        if ($request->file('file_review')) {
             $record->file_review = $request->file('file_review')->storeAs('/file_review', $record->innovation_title . "." . $request->file('file_review')->extension(), 'public');
-            $record->file_review = '/'.$record->file_review;
+            $record->file_review = '/' . $record->file_review;
         }
 
         // if($request->nonfin != null){
@@ -107,13 +130,13 @@ class BenefitController extends Controller
         //     }
         // }
         $record->status = 'upload benefit';
-        $record->updateAndHistory([] ,'update benefit form');
-        if (isset($request->bencus )) {
-            foreach($request->bencus as $index => $bc){
+        $record->updateAndHistory([], 'update benefit form');
+        if (isset($request->bencus)) {
+            foreach ($request->bencus as $index => $bc) {
                 PvtCustomBenefit::UpdateOrCreate([
                     'paper_id' => $id,
                     'custom_benefit_financial_id' => $index,
-                ],[
+                ], [
                     'value' => $bc
                 ]);
             }
@@ -128,55 +151,54 @@ class BenefitController extends Controller
         ]);
         // dd($validatedData['nonfin']);
 
-        if($record->status = 'upload benefit'){
-        // Pastikan relasi Team sudah dimuat dengan benar
-        if ($record->team) {
-            $fasilId = PvtMember::where('team_id', $record->team->id)
-                                ->where('status', 'facilitator')
-                                ->pluck('employee_id')
-                                ->first();
+        if ($record->status = 'upload benefit') {
+            // Pastikan relasi Team sudah dimuat dengan benar
+            if ($record->team) {
+                $fasilId = PvtMember::where('team_id', $record->team->id)
+                    ->where('status', 'facilitator')
+                    ->pluck('employee_id')
+                    ->first();
 
-            $fasilData = User::where('employee_id', $fasilId)
-                                ->select('name', 'email')
-                                ->first();
+                $fasilData = User::where('employee_id', $fasilId)
+                    ->select('name', 'email')
+                    ->first();
 
-            $leaderId = PvtMember::where('team_id', $record->team->id)
-                                ->where('status', 'leader')
-                                ->pluck('employee_id')
-                                ->first();
+                $leaderId = PvtMember::where('team_id', $record->team->id)
+                    ->where('status', 'leader')
+                    ->pluck('employee_id')
+                    ->first();
 
-            $leaderData = User::where('employee_id', $leaderId)
-                                ->select('name', 'email')
-                                ->first();
+                $leaderData = User::where('employee_id', $leaderId)
+                    ->select('name', 'email')
+                    ->first();
 
-            $inovasi_lokasi = Paper::where('id', $id)
-                                ->select('inovasi_lokasi')
-                                ->first();
+                $inovasi_lokasi = Paper::where('id', $id)
+                    ->select('inovasi_lokasi')
+                    ->first();
 
-            // Membuat objek EmailNotification
-            $mail = new EmailNotificationBenefit(
-                $record,
-                $record->status,
-                $record->innovation_title,
-                $record->team->team_name,
-                $leaderData,
-                $financial,
-                $potential_benefit,
-                $potensi_replikasi,
-                $non_financial,
-                $fasilData,
-                $inovasi_lokasi
-            );
+                // Membuat objek EmailNotification
+                $mail = new EmailNotificationBenefit(
+                    $record,
+                    $record->status,
+                    $record->innovation_title,
+                    $record->team->team_name,
+                    $leaderData,
+                    $financial,
+                    $potential_benefit,
+                    $potensi_replikasi,
+                    $non_financial,
+                    $fasilData,
+                    $inovasi_lokasi
+                );
 
-            //dd($fasilData);
+                //dd($fasilData);
 
-        // Mengirim email ke fasilitator
-        Mail::to($fasilData->email)->send($mail);
-
-        } else {
-            throw new \Exception('Paper tidak memiliki relasi dengan Team.');
+                // Mengirim email ke fasilitator
+                Mail::to($fasilData->email)->send($mail);
+            } else {
+                throw new \Exception('Paper tidak memiliki relasi dengan Team.');
+            }
         }
-    }
 
         return redirect()->route('paper.index')->with('success', 'Data berhasil diperbarui');
     }
