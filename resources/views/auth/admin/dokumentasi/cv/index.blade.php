8571a4321f910c6f5c3c2736d09247fc8187eb39
Arya.PK <aryakusuma0703@gmail.com>
2024-10-22 13:57:06 +0700

    lsdjlkfds

--- resources/views/auth/admin/dokumentasi/cv/index.blade.php
+++ resources/views/auth/admin/dokumentasi/cv/index.blade.php
@@ -246,11 +246,11 @@
     </div>
 
     {{-- Paggination --}}
-    <div class=" mt-2 d-flex justify-content-end">
+    <div class="pagination mt-2 d-flex justify-content-end">
         @if ($innovations->hasPages())
         {{ $innovations->links() }}
         @else
-        <ul class="mt-2 d-flex justify-content-end">
+        <ul class="pagination">
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
                 <span class="page-link" aria-hidden="true">&lsaquo;</span>
             </li>
