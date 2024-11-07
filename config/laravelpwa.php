eeb1614352a9f89d75299122581434cd4aa51dba
raflisetiawan <dev.raflisetiawan@gmail.com>
2024-11-07 16:32:42 +0700

    Update: change pwa icons

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,47 +1,47 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'SIG Innovation',
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
+        'name' => "SIG Innovation",
         'short_name' => 'PWA',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
         'display' => 'standalone',
-        'orientation'=> 'any',
-        'status_bar'=> 'black',
+        'orientation' => 'any',
+        'status_bar' => 'black',
         'icons' => [
             '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
+                'path' => '/images/icons/android/android-launchericon-72-72.png',
                 'purpose' => 'any'
             ],
             '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
+                'path' => '/images/icons/android/android-launchericon-96-96.png',
                 'purpose' => 'any'
             ],
             '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
+                'path' => '/images/icons/android/android-launchericon-128-128.png',
                 'purpose' => 'any'
             ],
             '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
+                'path' => '/images/icons/android/android-launchericon-144-144.png',
                 'purpose' => 'any'
             ],
             '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
+                'path' => '/images/icons/ios/152.png',
                 'purpose' => 'any'
             ],
             '192x192' => [
-                'path' => '/images/icons/icon-192x192.png',
+                'path' => '/images/icons/android/android-launchericon-192-192.png',
                 'purpose' => 'any'
             ],
             '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
+                'path' => '/images/icons/android/android-launchericon-384-384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
-                'path' => '/images/icons/icon-512x512.png',
+                'path' => '/images/icons/android/android-launchericon-512-512.png',
                 'purpose' => 'any'
             ],
         ],
