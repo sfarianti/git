5b9977028f6da24109211c1e62889d517f209ee6
raflisetiawan <dev.raflisetiawan@gmail.com>
2024-11-07 16:03:14 +0700

    Feat: PWA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>@yield('title') - SIG Innovation</title>
+    @laravelPWA
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css'])
