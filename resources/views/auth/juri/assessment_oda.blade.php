dd09ebfed0fc9deb285121dc42738cdb4257a9d0
Lenovoo\dennysyaputraaa <dennysyaputraa07@gmail.com>
2024-11-20 13:47:54 +0700

    change button color add judge & delete judge

--- resources/views/auth/juri/assessment_oda.blade.php
+++ resources/views/auth/juri/assessment_oda.blade.php
@@ -191,7 +191,7 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-danger" type="button" data-bs-dismiss="modal">Close</button>
                         <button class="btn btn-primary" type="submit">Submit</button>
                     </div>
                 </form>
@@ -222,7 +222,7 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-danger" type="button" data-bs-dismiss="modal">Close</button>
                         <button class="btn btn-primary" type="submit">Submit</button>
                     </div>
                 </form>
