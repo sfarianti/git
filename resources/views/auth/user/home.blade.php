ece57d2789d5af3b361879ccbeed610ae2afaf3a
Lenovoo\dennysyaputraaa <dennysyaputraa07@gmail.com>
2024-12-26 11:03:50 +0700

    add color in header akumulasi total benefit

--- resources/views/auth/user/home.blade.php
+++ resources/views/auth/user/home.blade.php
@@ -82,9 +82,10 @@
                 <div class="col-12 col-lg-6 col-md-8 dashboard-section">
                     <!-- Benefit Section -->
                     <div class="mb-4">
-                        <h4 class="mb-3">Akumulasi Total benefit</h4>
+                        <h4 class="mb-3" style="background-color: red; color: white; padding: 17px;">Akumulasi Total Benefit</h4>
                         <x-dashboard.benefit :year="$year" :is-superadmin="$isSuperadmin" :user-company-code="$userCompanyCode" />
                     </div>
+
                     <div class="mb-3">
                         <x-dashboard.total-financial-benefit-card />
                     </div>
