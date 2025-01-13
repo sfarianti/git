1c4edebf9cd4dbf9e6e19988bc5c81ee62c295e0
Lenovoo\dennysyaputraaa <dennysyaputraa07@gmail.com>
2025-01-13 08:04:38 +0700

    Update Change Language

--- resources/views/components/detail-company-chart/filter-by-organization-unit.blade.php
+++ resources/views/components/detail-company-chart/filter-by-organization-unit.blade.php
@@ -2,18 +2,17 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="filterModalLabel">Filter berdasarkan Unit Organisasi</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h5 class="modal-title" id="filterModalLabel">Filter Berdasarkan Unit Organisasi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
             </div>
             <div class="modal-body">
-                <form id="filterForm" action="{{ route('detail-company-chart-show', ['id' => $companyId]) }}"
-                    method="GET">
+                <form id="filterForm" action="{{ route('detail-company-chart-show', ['id' => $companyId]) }}" method="GET">
                     <div class="mb-3">
                         <label for="organizationLevel" class="form-label">Pilih Tingkat Organisasi</label>
                         <select class="form-select" id="organizationLevel" name="organization-unit">
-                            <option selected disabled>Select Organization Level</option>
+                            <option selected disabled>Pilih Tingkat Organisasi</option>
                             <option value="directorate_name">Direktorat</option>
-                            <option value="group_function_name">Group</option>
+                            <option value="group_function_name">Grup</option>
                             <option value="department_name">Departemen</option>
                             <option value="unit_name">Unit</option>
                             <option value="section_name">Seksi</option>
@@ -32,8 +31,8 @@
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary" form="filterForm">Apply Filter</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary" form="filterForm">Terapkan Filter</button>
             </div>
         </div>
     </div>
