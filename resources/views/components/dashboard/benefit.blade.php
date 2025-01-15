da2e21a434979c12a6e1ad67029e178b6e851b51
Lenovoo\dennysyaputraaa <dennysyaputraa07@gmail.com>
2025-01-15 14:05:58 +0700

    Update Formating and Recolor Chart in Dashboard Benefit

--- resources/views/components/dashboard/benefit.blade.php
+++ resources/views/components/dashboard/benefit.blade.php
@@ -1,9 +1,9 @@
 @vite(['resources/css/app.css'])
 
 <div class="card-header bg-gradient-primary d-flex justify-content-between align-items-center p-3 rounded">
-    <h4 class="text-white">Akumulasi Total Financial dan Potential Benefit</h4>
+    <h4 class="text-white">Akumulasi Total Benefit (Real & Potensial)</h4>
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle d-flex align-items-center" type="button" id="yearDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <button class="btn btn-pink dropdown-toggle d-flex align-items-center" type="button" id="yearDropdown" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="bi bi-calendar me-2"></i> <!-- Added margin-end for spacing -->
             {{ $year }}
         </button>
