9dc31fc74532000d447d06620c6918fc1b3f6341
Lenovoo\dennysyaputraaa <dennysyaputraa07@gmail.com>
2025-01-17 10:05:16 +0700

    Update add color in card Total team per perusahaan case in statistic and fix typo in chart tahap penilaian

--- resources/views/components/dashboard/event/total-team-card-company.blade.php
+++ resources/views/components/dashboard/event/total-team-card-company.blade.php
@@ -4,13 +4,20 @@
     </div>
     <div class="card-body">
         <div class="row">
+            <?php
+            // Define a set of light colors
+            $lightColors = [
+                '#F8D7DA', '#D1ECF1', '#D4EDDA', '#FFF3CD', '#C3E6CB', '#F9F9F9'
+            ];
+            ?>
             @foreach ($totalTeams as $teamData)
                 <div class="col-md-4 mb-3">
-                    <div class="card shadow-sm">
+                    <div class="card shadow-sm" style="background-color: {{ $lightColors[array_rand($lightColors)] }};">
                         <div class="card-body text-center">
                             <h5 class="card-title">{{ $teamData['company_name'] }}</h5>
                             <p class="card-text">
-                                <strong>Total Team:</strong> {{ $teamData['total_teams'] }}
+                                <strong>Total Team:</strong>
+                                <span style="font-size: 1.9rem; font-weight: bold;">{{ $teamData['total_teams'] }}</span>
                             </p>
                         </div>
                     </div>
